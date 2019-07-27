e44936d6d41e60cd97e8e288cf30bc5d501134e1
Samikshya Siwakoti <samikshya.siwakoti@gmail.com>
2019-07-27 09:29:09 +0545

    Created Main.py
    Git testing adding file to it.

---
+++ main.py
@@ -0,0 +1,25 @@
+number1 = 100
+list =  [4,5,6,7]
+
+a= 10
+b =5
+
+print(a<=b)
+
+
+# Dict = {'dog':'cute','cat': 'black'}
+#
+#
+
+
+
+for li in range(0,100,2):
+    print (li)
+
+
+#
+# while(a<=b):
+#     print('hello')
+
+
+import('./file.csv')
